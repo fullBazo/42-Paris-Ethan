c3498bb630b14e715b62b8543410f4a2fa2fb7f5
Ethan Huet <ehuet@f6r9s3.clusters.42paris.fr>
2025-11-05 16:46:05 +0100

    backup

---
+++ libft/ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehuet <ehuet@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 14:19:18 by ehuet             #+#    #+#             */
+/*   Updated: 2025/11/05 14:19:32 by ehuet            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == 45 || str[i] == 43)
+	{
+		if (str[i] == 45)
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (result * sign);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*str = "   ----+--54546q5485";
+
+	printf("%d\n", ft_atoi(str));
+	return (0);
+}
+*/
