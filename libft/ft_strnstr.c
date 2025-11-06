4c2bd5954e36315ed058d057150b1360f1a4ecd7
Ethan Huet <ehuet@f6r9s2.clusters.42paris.fr>
2025-11-06 11:12:51 +0100

    iurehwiuerg8uiyer

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,47 @@
 /*   By: ehuet <ehuet@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 17:14:02 by ehuet             #+#    #+#             */
-/*   Updated: 2025/11/05 18:39:54 by ehuet            ###   ########.fr       */
+/*   Updated: 2025/11/06 11:08:13 by ehuet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (*to_find == '\0')
+		return ((char *)str);
+
+	i = 0;
+	while (str[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (str[i + j] != '\0'
+			&& str[i + j] == to_find[j]
+			&& i + j < len)
+		{
+			if (to_find[j + 1] == '\0')
+				return ((char *)&str[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/*
+int     main(void)
+{
+        char    *str = "salut a tous";
+        char    *to_find = "lu";
+
+        printf("%s\n", strstr(str, to_find));
+        printf("%s\n", ft_strnstr(str, to_find, 5));
+        return (0);
+}
+*/
 
