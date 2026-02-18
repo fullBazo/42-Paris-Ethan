cd03d298a92cb448b96e99bcec64305460482d57
Ethan Huet <ehuet@f6r9s4.clusters.42paris.fr>
2026-02-18 16:05:45 +0100

    organized

--- Exam/Exam3/filter.c
+++ Exam/Exam3/filter.c
@@ -1,46 +1,65 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   filter.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ehuet <ehuet@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/13 11:58:15 by ehuet             #+#    #+#             */
-/*   Updated: 2026/02/13 13:22:20 by ehuet            ###   ########.fr       */
-/*                                                                            */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 filter.c											:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: ehuet <ehuet@student.42.fr>				+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2026/02/13 11:58:15 by ehuet			   #+#	  #+#			  */
+/*	 Updated: 2026/02/16 10:30:12 by ehuet			  ###	########.fr		  */
+/*																			  */
 /* ************************************************************************** */
 
 #define _GNU_SOURCE
 
 #include <string.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
 	if (!av[1] || ac != 2)
 		return 1;
 
-	char tmp[strlen(av[1])];
-	char *buf = malloc(sizeof(char) * 4096 + strlen(av[1]));
-	int	fd = 0, i = 0, tmp_len = 0, bytes_read = 1, total;
+	char *tmp = malloc(strlen(av[1]));
+	if (!tmp)
+	{
+		perror("Error : failed malloc allocation");
+		return 1;
+	}
+	char *buf = malloc(4096);
+	if (!buf)
+	{
+		perror("Error : failed malloc allocation");
+		return 1;
+	}
+	int tmp_len = 0, bytes_read = 1, total, pat_len = strlen(av[1]);
 
-	memset(tmp, 0, strlen(av[1]));
 	while (bytes_read > 0)
 	{
-		bytes_read = read(fd, buf, 4096);
+		bytes_read = read(0, buf, 4096);
+		if (bytes_read < 0)
+		{
+			perror ("Error : failed read on stdin");
+			return 1;
+		}
 		if (tmp_len > 0)
 		{
 			memmove(buf + tmp_len, buf, bytes_read);
 			memmove(buf, tmp, tmp_len);
 		}
 		total = tmp_len + bytes_read;
-		while (memmem(buf, total, av[1], strlen(av[1])))
+		char *match;
+		while ((match = memmem(buf, total, av[1], pat_len)) != NULL)
 		{
-			
+			for (int i = 0; i < pat_len; i++)
+				match[i] = '*';
 		}
+		write(1, buf, total - (pat_len - 1));
+		memmove(tmp, buf + total - (pat_len - 1), pat_len - 1);
+		tmp_len = pat_len - 1;
 	}
+	write(1, tmp, tmp_len);
 }
-
--- FdF/readme.txt
+++
@@ -1,7 +0,0 @@
-gcc -Wall -Wextra -Werror -g3 -I includes \
-    srcs/read.c libft/libft.a -o alloc_map_test
-
---> permet les units tests pour mes fonctions ( main a inclure dans le fichier directement(a voir pour link un main .c {ca a lair relou mdr}))
-
-
-Note pour la prochaine fois --> faire en sorte que ma fonction puisse bien allouer le double tableau correctement.
