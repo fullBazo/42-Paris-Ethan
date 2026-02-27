54bb67df389e8d2e0195cecca230c8aceee133d6
Ethan Huet <ehuet@f6r9s4.clusters.42paris.fr>
2026-02-27 13:12:01 +0100

    filter and gnl done

---
+++ Exam/Exam3/broken_gnl/get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+char    *get_next_line(int fd);
+
+#endif
---
+++ Exam/Exam3/filter/filter.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int	ft_strncmp(char *s1, char *s2, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i != n - 1 && s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int i = 0, j, r = 0, len = strlen(av[1]);
+		char c;
+		char str[70000];
+
+		r = read(0, &c, 1);
+		if (r <= 0)
+		{
+			perror("Error :");
+			return (1);
+		}
+		while (r)
+		{
+			str[i] = c;
+			i++;
+			r = read(0, &c, 1);
+		}
+		str[i] = 0;
+		i = 0;
+		while (str[i])
+		{
+			if (ft_strncmp(av[1], &str[i], len) == 0)
+			{
+				j = 0;
+				while (j != len)
+				{
+					write(1, "*", 1);
+					i++;
+					j++;
+				}
+			}
+			else
+				write(1, &str[i++], 1);
+		}
+	}
+	else
+		return (1);
+}
