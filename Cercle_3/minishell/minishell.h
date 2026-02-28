0d69d12211cabdfde16eae06e21f374bb3b5bedf
Ethan Huet <ehuet@f6r9s4.clusters.42paris.fr>
2026-02-28 11:50:27 +0100

    test

--- Cercle_3/minishell/minishell.h
+++ Cercle_3/minishell/minishell.h
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ehuet <ehuet@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/28 11:17:38 by ehuet             #+#    #+#             */
-/*   Updated: 2026/02/28 11:49:45 by ehuet            ###   ########.fr       */
+/*   Created: 2026/02/28 11:13:17 by ehuet             #+#    #+#             */
+/*   Updated: 2026/02/28 11:38:49 by ehuet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-int	main(void)
-{
-	rl_bind_key('\t', rl_complete);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <curses.h>
 
-	using_history();
-
-	while (1)
-	{
-		char *input = readline("minishell> ");
-		if (!input)
-			break ;
-
-		add_history(input);
-		free(input);
-	}
-	return (0);
-}
+#endif
